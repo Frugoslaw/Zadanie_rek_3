1db4a34afd30edb476033a158a1361b95a797a33
Frugoslaw <74188817+Frugoslaw@users.noreply.github.com>
2025-02-28 10:29:26 +0100

    Added API versioning with prefix api/v1

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,13 @@ use App\Http\Controllers\Api\MovieController;
 use App\Http\Controllers\Api\SerieController;
 use App\Http\Controllers\Api\GenreController;
 
-Route::get('movies', [MovieController::class, 'index']);
-Route::get('movies/{tmdbId}', [MovieController::class, 'show']);
+Route::prefix('api/v1')->group(function () {
+    Route::get('movies', [MovieController::class, 'index']);
+    Route::get('movies/{tmdbId}', [MovieController::class, 'show']);
 
-Route::get('series', [SerieController::class, 'index']);
-Route::get('series/{tmdbId}', [SerieController::class, 'show']);
+    Route::get('series', [SerieController::class, 'index']);
+    Route::get('series/{tmdbId}', [SerieController::class, 'show']);
 
-Route::get('genres', [GenreController::class, 'index']);
-Route::get('genres/{tmdbId}', [GenreController::class, 'show']);
+    Route::get('genres', [GenreController::class, 'index']);
+    Route::get('genres/{tmdbId}', [GenreController::class, 'show']);
+});
